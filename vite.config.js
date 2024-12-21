b3ca4b4788f75e6b1316891b6ec8358067ce8be8
shiva <shivangishree020@gmail.com>
2024-12-21 12:04:50 +0530

    config changed

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/Play-Codes",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Optional: Increase warning limit to 1000 kB
+  },
 })
